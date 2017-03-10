9331dcd2ae195ce36396d1c13cc737a58c1e8eb2
Akhil Raj Azhikdaon <akhil95raj@gmail.com>
2017-03-10 11:41:53 +0530

    gitignore added

---
+++ inputshark/news/archivednews.py
@@ -0,0 +1,13 @@
+import urllib
+import urllib2
+
+url = 'http://www.someserver.com/cgi-bin/register.cgi'
+
+values = {'name' : 'Michael Foord',
+          'location' : 'Northampton',
+          'language' : 'Python' }
+
+data = urllib.urlencode(values)
+req = urllib2.Request(url, data)
+response = urllib2.urlopen(req)
+the_page = response.read()
